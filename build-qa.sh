7736fee40b891c1564ea7adc36c1cba5c0257576
Anirudh Rao <anirudh.rao@globalquakemodel.org>
2015-08-01 11:56:13 +0200

    SR: cases_1b..d hand calculations and tables

--- build-qa.sh
+++ build-qa.sh
@@ -1,8 +1,12 @@
-pdflatex -interaction=nonstopmode oq-risk-qa.tex
+pdflatex -interaction=nonstopmode oq-risk-qa.tex > qa-tex.log
 makeindex oq-risk-qa.idx -s configuration/StyleInd.ist
-bibtex oq-risk-qa
-makeglossaries oq-risk-qa
-pdflatex -interaction=nonstopmode oq-risk-qa.tex
-pdflatex -interaction=nonstopmode oq-risk-qa.tex
+bibtex oq-risk-qa > qa-bib.log
+makeglossaries oq-risk-qa > qa-glos.log
+pdflatex -interaction=nonstopmode oq-risk-qa.tex > qa-tex.log
+pdflatex -interaction=nonstopmode oq-risk-qa.tex > qa-tex.log
+cat qa-tex.log | grep "Error"
+cat qa-tex.log | grep "Warning"
+cat qa-bib.log | grep "found"
+cat qa-glos.log | grep "Warning"
 ./clean.sh
 open oq-risk-qa.pdf
